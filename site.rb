cd2e58fe8c9a95f1420e2e8d69298d5bbc710f9d
geoffgough <geoffrey.gough@utah.edu>
2014-06-01 18:19:26 +0000

    Another Version

--- site.rb
+++ site.rb
@@ -1,9 +1,6 @@
- require 'sinatra'
+require 'sinatra'
  set :port, 4000
  set :bind, '0.0.0.0'
  get '/' do
-   erb :index
-     end
- get '/italian' do
-     "Ciao"
-     end
+   "I'm alive!"
+ end
